d5878abbff49808c3e6da79f122cc9bb1efb54ac
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-02 18:24:43 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -3,7 +3,7 @@ logger = logging.getLogger(__name__)
 
 from pyrogram import Client, filters
 from bot import Star_Moviess_Tamil
-from config import ADMINS, AUTH_USERS
+from config import config
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 from translation import Translation
 from pyrogram.errors import MessageNotModified, UserIsBlocked, InputUserDeactivated, FloodWait
@@ -15,18 +15,9 @@ import traceback
 from pyrogram import Client
 from pyrogram import StopPropagation, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
-
 import config
-from handlers.broadcast import broadcast
-from handlers.check_user import handle_user_status
-from handlers.database import Database
 
 LOG_CHANNEL = config.LOG_CHANNEL
-AUTH_USERS = config.AUTH_USERS
-DB_URL = config.DB_URL
-DB_NAME = config.DB_NAME
-
-db = Database(DB_URL, DB_NAME)
 
 ################################################################################################################################################################################################################################################
 # Start Command
@@ -208,190 +199,3 @@ async def about(client, message):
         reply_markup=reply_markup,
         disable_web_page_preview=True
     )
-
-################################################################################################################################################################################################################################################
-
-REPLY_ERROR = """<b>Use This Command as a Reply to any Telegram Message Without any Spaces.</b>"""
-
-################################################################################################################################################################################################################################################
-# Bot Settings
-
-@Star_Moviess_Tamil.on_message(filters.command("settings"))
-async def opensettings(bot, cmd):
-    user_id = cmd.from_user.id
-    await cmd.reply_text(
-        f"**Here You Can Set Your Settings :-\n\nSuccessfully setted Notifications to {await db.get_notif(user_id)}**",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
-                        callback_data="notifon",
-                    )
-                ],
-                [InlineKeyboardButton("❎", callback_data="closeMeh")],
-            ]
-        ),
-    )
-
-################################################################################################################################################################################################################################################
-# Broadcast Message 
-
-@Star_Moviess_Tamil.on_message(filters.private & filters.command("broadcast"))
-async def broadcast_handler_open(_, m):
-    if m.from_user.id not in AUTH_USERS:
-        await m.delete()
-        return
-    if m.reply_to_message is None:
-        await m.reply(REPLY_ERROR)
-    else:
-        await broadcast(m, db)
-
-################################################################################################################################################################################################################################################
-# Total Users in Database 📂
-
-@Star_Moviess_Tamil.on_message(filters.private & filters.command("stats"))
-async def sts(c, m):
-    if m.from_user.id not in AUTH_USERS:
-        await m.delete()
-        return
-    await m.reply_text(
-        text=f"**Total Users in Database 📂 :- {await db.total_users_count()}\n\nTotal Users with Notification Enabled 🔔 :- {await db.total_notif_users_count()}**",
-        quote=True
-    )
-
-################################################################################################################################################################################################################################################
-# Ban The User
-
-@Star_Moviess_Tamil.on_message(filters.private & filters.command("ban_user"))
-async def ban(c, m):
-    if m.from_user.id not in AUTH_USERS:
-        await m.delete()
-        return
-    if len(m.command) == 1:
-        await m.reply_text(
-            f"**Use This Command to Ban 🛑 any User From the Bot 🤖.\n\nUsage:-\n\n/ban_user user_id ban_duration ban_reason\n\n Example :- /ban_user 1234567 28 You Misused me.\n This Will Ban User with ID `1234567` for `28` Days for the Reason `You Misused me`.**",
-            quote=True,
-        )
-        return
-
-    try:
-        user_id = int(m.command[1])
-        ban_duration = int(m.command[2])
-        ban_reason = " ".join(m.command[3:])
-        ban_log_text = f"**Banning user {user_id} for {ban_duration} Days for the Reason {ban_reason}.**"
-
-        try:
-            await c.send_message(
-                user_id,
-                f"**You are Banned 🚫 to Use This Bot for {ban_duration} day(s) for the reason __{ban_reason}__ \n\nMessage from the Admin 🤠**",
-            )
-            ban_log_text += "**\n\nUser Notified Successfully!!**"
-        except BaseException:
-            traceback.print_exc()
-            ban_log_text += (
-                f"**\n\n ⚠️ User Notification Failed! ⚠️ \n\n`{traceback.format_exc()}`**"
-            )
-        await db.ban_user(user_id, ban_duration, ban_reason)
-        print(ban_log_text)
-        await m.reply_text(ban_log_text, quote=True)
-    except BaseException:
-        traceback.print_exc()
-        await m.reply_text(
-            f"**Error Occurred ⚠️! Traceback Given below\n\n`{traceback.format_exc()}`**",
-            quote=True
-        )
-
-################################################################################################################################################################################################################################################
-# Unban User
-
-@Star_Moviess_Tamil.on_message(filters.private & filters.command("unban_user"))
-async def unban(c, m):
-    if m.from_user.id not in AUTH_USERS:
-        await m.delete()
-        return
-    if len(m.command) == 1:
-        await m.reply_text(
-            f"**Use this Command to Unban 😃 Any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.**",
-            quote=True,
-        )
-        return
-
-    try:
-        user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user 🤪 {user_id}"
-
-        try:
-            await c.send_message(user_id, f"Your ban was lifted!")
-            unban_log_text += "**\n\n✅ User Notified Successfully!! ✅**"
-        except BaseException:
-            traceback.print_exc()
-            unban_log_text += (
-                f"**\n\n⚠️ User Notification Failed! ⚠️\n\n`{traceback.format_exc()}`**"
-            )
-        await db.remove_ban(user_id)
-        print(unban_log_text)
-        await m.reply_text(unban_log_text, quote=True)
-    except BaseException:
-        traceback.print_exc()
-        await m.reply_text(
-            f"**⚠️ Error Occurred ⚠️! Traceback Given below\n\n`{traceback.format_exc()}`**",
-            quote=True,
-        )
-
-################################################################################################################################################################################################################################################
-# Banned Users
-
-@Star_Moviess_Tamil.on_message(filters.private & filters.command("banned_users"))
-async def _banned_usrs(c, m):
-    if m.from_user.id not in AUTH_USERS:
-        await m.delete()
-        return
-    all_banned_users = await db.get_all_banned_users()
-    banned_usr_count = 0
-    text = ""
-    async for banned_user in all_banned_users:
-        user_id = banned_user["id"]
-        ban_duration = banned_user["ban_status"]["ban_duration"]
-        banned_on = banned_user["ban_status"]["banned_on"]
-        ban_reason = banned_user["ban_status"]["ban_reason"]
-        banned_usr_count += 1
-        text += f"> **User ID :- `{user_id}`, Ban Duration :- `{ban_duration}`, Banned on :- `{banned_on}`, Reason :- `{ban_reason}`\n\n**"
-    reply_text = f"**Total banned user(s) 🤭: `{banned_usr_count}`\n\n{text}**"
-    if len(reply_text) > 4096:
-        with open("banned-users.txt", "w") as f:
-            f.write(reply_text)
-        await m.reply_document("banned-users.txt", True)
-        os.remove("banned-users.txt")
-        return
-    await m.reply_text(reply_text, True)
-
-
-@Star_Moviess_Tamil.on_callback_query()
-async def callback_handlers(bot: Client, cb: CallbackQuery):
-    user_id = cb.from_user.id
-    if cb.data == "notifon":
-        notif = await db.get_notif(cb.from_user.id)
-        if notif is True:
-            await db.set_notif(user_id, notif=False)
-        else:
-            await db.set_notif(user_id, notif=True)
-        await cb.message.edit(
-            f"**Here You Can Set Your Settings :-\n\nSuccessfully setted Notifications to {await db.get_notif(user_id)}**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
-                            callback_data="notifon",
-                        )
-                    ],
-                    [InlineKeyboardButton("❎", callback_data="closeMeh")],
-                ]
-            ),
-        )
-        await cb.answer(
-            f"**Successfully setted Notifications to {await db.get_notif(user_id)}**"
-        )
-    else:
-        await cb.message.delete(True)

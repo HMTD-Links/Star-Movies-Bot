89e5e6f1c4d0635aa13e1504dbe47a15e943b138
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-03 06:37:54 +0530

    Update movie_poster.py

--- Plugins/movie_poster.py
+++ Plugins/movie_poster.py
@@ -1,5 +1,21 @@
+import logging
+logger = logging.getLogger(__name__)
 
+from pyrogram import Client, filters
+from bot import Star_Moviess_Tamil
+from config import Config
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
+from movies import Movies
+from pyrogram.errors import MessageNotModified, UserIsBlocked, InputUserDeactivated, FloodWait
+import random
+import os
+import asyncio
+import traceback
 
+from pyrogram import Client
+from pyrogram import StopPropagation, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+import config
 
 ################################################################################################################################################################################################################################################
 
@@ -8,9 +24,9 @@
 ################################################################################################################################################################################################################################################
 
 @Star_Moviess_Tamil.on_message(filters.command("alien_covenant") & filters.private & filters.incoming)
-async def start(client, message):
+async def alien_covenant(client, message):
     await message.reply_photo(
-        caption = Translation.ALIEN_COVENANT.format(
+        caption = MOVIES.ALIEN_COVENANT.format(
                 mention = message.from_user.mention
             ),
         photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",

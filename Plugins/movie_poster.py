66d4edb063ae1ede54aa90f5bdf9a9e9d0e2ad2a
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-03 10:58:05 +0530

    Update movie_poster.py

--- Plugins/movie_poster.py
+++ Plugins/movie_poster.py
@@ -24,14 +24,132 @@ import config
 ################################################################################################################################################################################################################################################
 # Alien Covenant (2017)
 
-@Star_Moviess_Tamil.on_message(filters.command("alien_covenant") & filters.private & filters.incoming)
+@Star_Moviess_Tamil.on_message(filters.command("alien_covenant1") & filters.private & filters.incoming)
 async def alien_covenant(client, message):
     await message.reply_photo(
-        caption = MOVIES.ALIEN_COVENANT.format(
+        caption = Movies.ALIEN_COVENANT.format(
                 mention = message.from_user.mention
             ),
         photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",
         quote=True,
     )
+
+################################################################################################################################################################################################################################################
+
+################################################################################################################################################################################################################################################
+
+                  # Star Movies Tamil
+
+################################################################################################################################################################################################################################################
+# Alien Covenant (2017)
+
+ALIEN_COVENANT = "Movies.ALIEN_COVENANT"
+
+MOVIE_BUTTONS = [
+            [
+                InlineKeyboardButton('📃 1/5', callback_data="pages"),
+                InlineKeyboardButton('Next ➡️', callback_data="page 2")
+            ]
+        ]
+
+@Star_Moviess_Tamil.on_message(filters.command("alien_covenant") & filters.private & filters.incoming)
+async def alien_covenant(client, message):
+    await message.reply_photo(
+        caption = Movies.ALIEN_COVENANT.format(
+                mention = message.from_user.mention
+            ),
+        reply_markup=reply_markup,
+        photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",
+        quote=True
+    )
+    raise StopPropagation
+
+@Star_Moviess_Tamil.on_callback_query()
+async def callback_query(client: Client, query: CallbackQuery):
+    if query.data=="page 2":
+        MOVIE_PAGE2_BUTTONS = [
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="page 1"),
+                InlineKeyboardButton('📃 2/5', callback_data="pages"),
+                InlineKeyboardButton('Next ➡️', callback_data="page 3")
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(MOVIE_PAGE2_BUTTONS)
+        try:
+            await query.edit_message_photo(
+        caption = Movies.ALIEN_COVENANT_PAGE2.format(
+                mention = message.from_user.mention
+            ),
+        reply_markup=reply_markup,
+        photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",
+        quote=True
+    )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="page 3":
+        MOVIE_PAGE3_BUTTONS = [
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="page 2"),
+                InlineKeyboardButton('📃 3/5', callback_data="pages"),
+                InlineKeyboardButton('Next ➡️', callback_data="page 4")
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(MOVIE_PAGE3_BUTTONS)
+        try:
+            await query.edit_message_photo(
+        caption = Movies.ALIEN_COVENANT_PAGE3.format(
+                mention = message.from_user.mention
+            ),
+        reply_markup=reply_markup,
+        photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",
+        quote=True
+    )
+        except MessageNotModified:
+            pass    
+
+    elif query.data=="page 4":
+        MOVIE_PAGE4_BUTTONS = [
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="page 3"),
+                InlineKeyboardButton('📃 4/5', callback_data="pages"),
+                InlineKeyboardButton('Next ➡️', callback_data="page 5")
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(MOVIE_PAGE4_BUTTONS)
+        try:
+            await query.edit_message_photo(
+        caption = Movies.ALIEN_COVENANT_PAGE4.format(
+                mention = message.from_user.mention
+            ),
+        reply_markup=reply_markup,
+        photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",
+        quote=True
+    )
+        except MessageNotModified:
+            pass      
+          
+    elif query.data=="page 5":
+        MOVIE_PAGE5_BUTTONS = [
+            [
+                InlineKeyboardButton("⬅️ BACK", callback_data="page 4"),
+                InlineKeyboardButton('📃 5/5', callback_data="pages"),
+            ]
+        ]
+
+        reply_markup = InlineKeyboardMarkup(MOVIE_PAGE5_BUTTONS)
+        try:
+            await query.edit_message_photo(
+        caption = Movies.ALIEN_COVENANT_PAGE5.format(
+                mention = message.from_user.mention
+            ),
+        reply_markup=reply_markup,
+        photo="https://telegra.ph/file/206f9013802376b39ad03.jpg",
+        quote=True
+    )
+        except MessageNotModified:
+            pass    
+        return
+
 
 ################################################################################################################################################################################################################################################
